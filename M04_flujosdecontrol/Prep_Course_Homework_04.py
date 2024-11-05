d9777b170cf3778c45250fc95c44b4f43297caf5
Danna <jimenezcastrodanna6@gmail.com>
2024-11-05 16:35:05 -0500

    Estructuras de control :c

--- M04_flujosdecontrol/Prep_Course_Homework_04.py
+++ M04_flujosdecontrol/Prep_Course_Homework_04.py
@@ -7,7 +7,13 @@
 
 # In[4]:
 
-
+a = 56
+if (a < 0):
+    print('Este número es menor a 0')
+elif (a > 0):
+    print('Este número es mayor que 0')
+else:
+    print('El número es igual a 0')
 
 
 
@@ -15,72 +21,111 @@
 
 # In[5]:
 
-
-
+x = 'Danna'
+y = 35
+if (type(x) == type(y)):
+    print ('Las variables son tipos de datos iguales')
+else:
+    print('Las variables son tipos de datos diferentes')
 
 
 # 3) Para los valores enteros del 1 al 20, imprimir por pantalla si es par o impar
 
 # In[7]:
 
-
-
+for i in range (1, 20):
+    if i % 2 ==0:
+        print(f'El número es, {i} par')
+    else:
+        print(f'El número es, {i} impar')
 
 
 # 4) En un ciclo for mostrar para los valores entre 0 y 5 el resultado de elevarlo a la potencia igual a 3
 
 # In[9]:
 
-
-
-
+for i in range (0, 5):
+    print(f'Número', {i}, 'Elevado a la 3', {i**3})
 
 # 5) Crear una variable que contenga un número entero y realizar un ciclo for la misma cantidad de ciclos
 
 # In[10]:
 
-
-
-
+a = 56
+for i in range (0, a):
+    pass
+print(a) 
 
 # 6) Utilizar un ciclo while para realizar el factoreo de un número guardado en una variable, sólo si la variable contiene un número entero mayor a 0
 
 # In[33]:
 
-
-
+x = 45
+if x > 0:
+    factorial = 1
+    contador = x
+    while contador > 0:
+        factorial *= contador
+        contador -= 1
+        print(f"El factorial de {x} es {factorial}")
+else:
+    print("La variable debe ser un número entero mayor a 0.")
 
 
 # 7) Crear un ciclo for dentro de un ciclo while
 
 # In[38]:
 
-
-
-
+contador = 0 
+while contador < 3:
+    print(f'Ciclo while: {contador + 1}')
+    for i in range(1, 4):
+        print(f'  Iteración del ciclo for: {i}')
+    contador += 1
 
 # 8) Crear un ciclo while dentro de un ciclo for
 
 # In[3]:
 
-
-
-
+for i in range(1, 4):
+    print(f'Ciclo for: {i}')
+    contador = 1
+    while contador <= 2:
+        print(f'Ciclo while: {contador}')
+        contador += 1
 
 # 9) Imprimir los números primos existentes entre 0 y 30
 
 # In[54]:
 
-
-
+tope_rango=30
+n = 2
+primo = True
+while (n < tope_rango):
+    for div in range(2, n):
+        if (n % div == 0):
+            primo = False
+    if (primo):
+        print(f'Este número es primo: {n}')
+    else:
+        primo = True
+    n += 1
 
 # 10) ¿Se puede mejorar el proceso del punto 9? Utilizar las sentencias break y/ó continue para tal fin
 
 # In[55]:
 
-
-
-
+tope_rango = 30
+n = 2
+while (n < tope_rango):
+    primo = True
+    for div in range(2, n):
+        if n % div == 0:
+            primo = False
+            break
+    if primo:
+        print(f'Este número es primo: {n}')
+    n += 1
 
 # 11) En los puntos 9 y 10, se diseño un código que encuentra números primos y además se lo optimizó. ¿Es posible saber en qué medida se optimizó?
 
@@ -98,9 +143,13 @@
 
 # In[62]:
 
-
-
-
+x = 100
+while (x <= 300):
+    if x % 12:
+        x += 1
+        continue
+    print (x)
+    x += 1 
 
 # 13) Utilizar la función **input()** que permite hacer ingresos por teclado, para encontrar números primos y dar la opción al usario de buscar el siguiente
 
@@ -113,5 +162,9 @@
 
 # In[75]:
 
-
-
+z = 100
+while z <= 300:
+    if z % 6 == 0:
+        print (f'El número en el rango de 100 a 300 es divisible por 3 y multiplo de 6: {z}')
+        break
+    z += 1 
